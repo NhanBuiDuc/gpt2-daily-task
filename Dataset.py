e3b48b8d28280ed523608a93f1f7898bad8579e6
Bui Duc Nhan <nhanbuiduc.work@gmail.com>
2023-08-17 04:09:27 +0700

    fix train.py, dataset

--- Dataset.py
+++ Dataset.py
@@ -1,36 +1,6 @@
 from torch.utils.data import Dataset
 import json
 
-# class ChatData(Dataset):
-#     def __init__(self, path:str, tokenizer):
-#         self.data = json.load(open(path, "r"))
-
-#         self.X = []
-#         for i in self.data:
-#             for j in i['dialog']:
-#                 self.X.append(j['text'])
-
-#         for idx, i in enumerate(self.X):
-#             try:
-#                 self.X[idx] = "<startofstring> "+i+" <bot>: "+self.X[idx+1]+" <endofstring>"
-#             except:
-#                 break
-
-#         self.X = self.X[:5000]
-        
-#         print(self.X[0])
-
-#         self.X_encoded = tokenizer(self.X,max_length=40, truncation=True, padding="max_length", return_tensors="pt")
-#         self.input_ids = self.X_encoded['input_ids']
-#         self.attention_mask = self.X_encoded['attention_mask']
-
-#     def __len__(self):
-#         return len(self.X)
-
-#     def __getitem__(self, idx):
-#         return (self.input_ids[idx], self.attention_mask[idx])
-    
-
 class DailyTaskData(Dataset):
     def __init__(self, path:str, tokenizer):
         self.data = json.load(open(path, "r"))
@@ -71,7 +41,7 @@ class DailyTaskData(Dataset):
             result_str += f"<{key}>{value}"
         return result_str
     
-class DailyTaskSequenceData(Dataset):
+class PromptResultMergedDataset(Dataset):
     def __init__(self, path:str, tokenizer):
         self.data = json.load(open(path, "r"))
         self.tokenizer = tokenizer
@@ -115,4 +85,31 @@ class DailyTaskSequenceData(Dataset):
             "input_ids": merge_ids,
             # "attention_mask": attention_mask,
             # "labels": target
+        }
+
+
+class DailyTaskDataset(Dataset):
+    def __init__(self, target_texts: list[str], tokenizer):
+        self.data = target_texts
+        self.tokenizer = tokenizer
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        entry = self.data[idx]
+        entry = "<startofstring>" + entry + "<endofstring>"
+        # Encode the target
+        encoding = self.tokenizer(
+            entry,
+            max_length=50,  # Adjust as needed
+            return_tensors="pt",
+            truncation=True
+        )
+
+        input_ids = encoding['input_ids']  # Use 'input_ids' for language modeling
+        attention_mask = encoding['attention_mask']
+        return {
+            "input_ids": input_ids,
+            "attention_mask": attention_mask,
         }
---
+++ train.py
@@ -0,0 +1,94 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from dataset import PromptResultMergedDataset
+from torch.optim import Adam, AdamW
+from torch.utils.data import DataLoader
+import tqdm
+import torch
+from transformers import Trainer, TrainingArguments, DataCollatorForLanguageModeling
+# Example usage
+input_entry = {
+    "prompt": "Prepare presentation for tomorrow's conference",
+}
+
+
+def train(dataset, model, max_length, temperature):
+    training_args = TrainingArguments(
+        output_dir="./output",
+        num_train_epochs=1000,
+        per_device_train_batch_size=1,
+        save_steps=10,
+        save_total_limit=2,
+        overwrite_output_dir=True,
+    )
+
+    data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False)
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=dataset
+    )
+
+    # Start training
+    trainer.train()
+
+    torch.save(model.state_dict(), "model_state.pt")
+    print(infer(input_entry, max_length))
+
+
+def infer(entry, max_length):
+    prompt = entry["prompt"]
+    input_text = f"<startofstring><startofprompt>{prompt}<endofpromt><endofstring>"
+    input_encoded = tokenizer(input_text, max_length=100, truncation=True, padding="max_length", return_tensors="pt")
+    input_ids = input_encoded['input_ids'].to("cuda")
+    attention_mask = input_encoded['attention_mask'].to("cuda")
+    
+    # Generate text with temperature and top-k sampling
+    temperature = 0.2  # Adjust this value
+    top_k = 50  # Adjust this value
+    output = model.generate(
+        input_ids,
+        attention_mask=attention_mask,
+        max_length=max_length,
+        temperature=temperature,
+        top_k=top_k,
+    )
+    generated_text = tokenizer.decode(output[0], skip_special_tokens=False)
+    
+    return generated_text
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+custom_tokens = [
+    "<startofprompt>", "<startoftask>",
+    "<endofpromt>", "<endoftask>", 
+    'sum', 'cate', 'prio', 'diff', 'imp', 'status', 'exp_min', 
+    'totd', 'spec_time', 'dow', 'day', 'month', 'no_date', 'no_week', 'no_month'
+]
+
+special_tokens = {
+    "pad_token": "<pad>",
+    "bos_token": "<startofstring>",
+    "eos_token": "<endofstring>",
+    "additional_special_tokens": custom_tokens
+}
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer.add_special_tokens(special_tokens)
+
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+model.resize_token_embeddings(len(tokenizer))
+
+model = model.to(device)
+
+dailyTaskDataset = PromptResultMergedDataset("./daily_task_data.json", tokenizer)
+dailyTaskDataLoader = DataLoader(dailyTaskDataset, batch_size=20)
+
+model.train()
+
+optim = Adam(model.parameters(), lr=1e-2)
+
+print("training .... ")
+temperature = 0.2
+max_length = 50
+train(dailyTaskDataset, model, max_length, temperature)
